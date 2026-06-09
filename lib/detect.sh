d90124de5b5ee366410bcec7e52238b5f730c10b
lucasandresc <lucas.andres.cabrera@gmail.com>
2026-06-09 02:30:57 -0300

    feat: add detect.sh with distro, package manager and init system detect

---
+++ lib/detect.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+check_distro() {
+  source /etc/os-release
+  export DISTRO=$(grep "^NAME=" /etc/os-release | cut -d= -f2 | tr -d '''')
+}
+
+check_package_manager() {
+  packages_manager=("paru" "yay" "pacman" "apt" "dnf" "zypper" "xbps-install" "apk" "emerge" "nix")
+  for manager in "${packages_manager[@]}"; do
+    if command -v "$manager" &>/dev/null; then
+      export PACKAGE_MANAGER="$manager"
+      break
+    fi
+  done
+}
+
+check_init_system() {
+  local SYSTEM
+  SYSTEM=$(ps -p 1 -o comm=)
+
+  case $SYSTEM in
+
+  systemd | openrc | init)
+    export INIT_SYSTEM="$SYSTEM"
+    ;;
+  *)
+    export INIT_SYSTEM="unknown"
+    ;;
+
+  esac
+
+}
+
+check_distro
+check_package_manager
+check_init_system
